e8b89d78094b79a8ccd7106e2c88bdf7fdf49cfc
nakyeong <rinarina0429@naver.com>
2024-12-21 22:25:18 +0900

    feat: 도서 검색 서비스 로직

--- src/main/java/com/gdgoc/Online_Library/book/BookService.java
+++ src/main/java/com/gdgoc/Online_Library/book/BookService.java
@@ -1,7 +1,9 @@
 package com.gdgoc.Online_Library.book;
 
 import com.gdgoc.Online_Library.book.dto.BookGetResponse;
+import com.gdgoc.Online_Library.book.dto.BooksGetResponse;
 import jakarta.persistence.EntityNotFoundException;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -23,5 +25,17 @@ public class BookService {
     public BookGetResponse getBookInfo(Long bookId) {
         Book book = getBookOrException(bookId);
         return BookGetResponse.of(book);
+    }
+
+    @Transactional(readOnly = true)
+    public BooksGetResponse searchBookWithTitle(String query) {
+        List<Book> books = bookRepository.findByTitleContaining(query);
+        return BooksGetResponse.of(books);
+    }
+
+    @Transactional(readOnly = true)
+    public BooksGetResponse searchBookWithAuthor(String query) {
+        List<Book> books = bookRepository.findByAuthorContaining(query);
+        return BooksGetResponse.of(books);
     }
 }
