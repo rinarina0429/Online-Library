edc3b067f4ba35bace0eb8abfe2536a7b4751c68
nakyeong <rinarina0429@naver.com>
2024-12-21 16:51:30 +0900

    fix: name 아닌 unique값인 email 받도록 dto 수정

--- src/main/java/com/gdgoc/Online_Library/auth/dto/LoginRequest.java
+++ src/main/java/com/gdgoc/Online_Library/auth/dto/LoginRequest.java
@@ -1,12 +1,15 @@
 package com.gdgoc.Online_Library.auth.dto;
 
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 @Data
 public class LoginRequest {
-    @NotBlank(message = "Name is required")
-    private String name;
+
+    @Email(message = "Invalid email address")
+    @NotBlank(message = "Email is required")
+    private String email;
 
     @NotBlank(message = "Password is required")
     private String password;
--- src/main/java/com/gdgoc/Online_Library/auth/dto/SignUpRequest.java
+++ src/main/java/com/gdgoc/Online_Library/auth/dto/SignUpRequest.java
@@ -1,13 +1,19 @@
 package com.gdgoc.Online_Library.auth.dto;
 
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 @Data
 public class SignUpRequest {
+
     @NotBlank(message = "Name is required")
     private String name;
 
     @NotBlank(message = "Password is required")
     private String password;
+
+    @Email(message = "Invalid email address")
+    @NotBlank(message = "Email is required")
+    private String email;
 }
