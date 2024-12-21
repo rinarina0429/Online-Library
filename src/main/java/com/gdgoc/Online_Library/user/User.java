3df7a7a3dd61862d34f3d9a23efb97a78fc90e6f
nakyeong <rinarina0429@naver.com>
2024-12-21 16:47:45 +0900

    remove: User 엔티티에 @Email 어노테이션 제거

--- src/main/java/com/gdgoc/Online_Library/user/User.java
+++ src/main/java/com/gdgoc/Online_Library/user/User.java
@@ -8,7 +8,6 @@ import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.validation.constraints.Email;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
@@ -31,7 +30,6 @@ public class User {
     @Column(nullable = false)
     private String password;
 
-    @Email
     @Column(unique = true, nullable = false)
     private String email;
 
